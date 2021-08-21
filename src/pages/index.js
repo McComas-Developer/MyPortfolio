b17ffe4f6af75fb6a05e7cfecad547d54cae6dd8
Michael <michael.ray.mccomas@gmail.com>
2021-08-20 21:28:18 -0500

    added new job. revised bio

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,16 +12,15 @@ const IndexPage = () => (
     <div className="Hero">
       <div className="HeroGroup">
         <h1>Michael McComas</h1>
-        <p>Computer Scientist and Android Developer</p>
+        <p>Computer Scientist, Eagle Scout, and Android Developer at FortyAU</p>
         <Wave />
       </div>
       <div className="AboutSection" id={"about"}>
         <h2>About</h2>
         <p>
-          Hey there! My name is Michael McComas and I'm an computer scientist studying at Middle Tennessee State University. 
-        I would be getting a degree in android development but unfortunately it wasn't an option  :D. 
-        So instead, I'll have to let my personal projects speak for themselves. I've learned a lot throughout my android experience,
-         but I am always on the lookout to learn more!
+          Hey there! My name is Michael McComas and I'm an computer scientist graduate from MTSU and an Android Developer at FortyAU. 
+          I am a super outdoorsy person with a lot of positivity. I am trying everything I can to improve my Mobile Dev skills and to help others so 
+          don't be afraid to reach out! :)
         </p>
       </div>
       <div className="Cards" id={"projects"}>
@@ -53,6 +52,18 @@ const IndexPage = () => (
       <div className="ExperienceSection" id={"experience"} >
         <h2>Experience</h2>
         <ExperienceCell
+          image={require("../images/logo_fortyau.jpg")}
+          company="FortyAU"
+          position="Android Developer"
+          time="Jun 2021 - Present"
+          details="Mid-June of 2021 I began working with a software development consulting company working on several projects.
+          From working on an Asurion health application, creating a fitness app from the ground up with a grant from the national
+          science foundation, to working with Cirrus to maintain their IOS application. Our development teams are all 'mentor-minded'
+          meaning that when someone is unfamiliar with a topic but someone else is, they work together to help them understand. I've 
+          had the opportunity to implement features such as Audio detection, creating a fitness app from the ground up, and creating
+          multi-language support."
+        />
+        <ExperienceCell
           image={require("../images/logo_android.jpg")}
           company="Android Freelance"
           position="Android Developer"
@@ -66,8 +77,8 @@ const IndexPage = () => (
           image={require("../images/logo_prgx.jpg")}
           link="https://www.prgx.com"
           company="PRGX"
-          position="IT Programmer"
-          time="Jun 2019 - Present"
+          position="Data Developer"
+          time="Jun 2019 - Jun 2021"
           details="In early june of 2019 I had the opportunity to work with PRGX to create 
                   new in-house applications. I used Microsoft access and SQL databases to create
                   dynamic tools to track, report, view, and analyze data. I had the honor of having 
